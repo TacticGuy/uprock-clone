21811a9f901659cd6b59804764984d021e4db46f
Sai Praveen <saimoturi402@gmail.com>
2026-01-08 12:41:42 +0530

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: ProcessingInstruction.env.VITE_BASE_PATH || "/uprock-clone",
+  base: process.env.VITE_BASE_PATH || "/uprock-clone",
   build: {
     minify: false,
     sourcemap: false,
