0c29c70183652d64c51fb6a481cc1812d3ee3628
Sai Praveen <saimoturi402@gmail.com>
2026-01-08 16:26:10 +0530

    FIX : 9898

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base:process.env.VITE_BASE_PATH || "/uprock-clone",
   build: {
     minify: false,
     sourcemap: false,
